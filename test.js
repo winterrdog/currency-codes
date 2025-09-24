1476104c8aa999ec9a83a1a199caa328bf9b9570
winterrdog <winterrdog@protonmail.ch>
2025-09-24 22:22:05 +0300

    Refactor test.js to use ES module syntax for imports and update assertions

--- test.js
+++ test.js
@@ -1,14 +1,22 @@
-var cc = require('./index');
-var assert = require('assert');
+import assert from "assert";
+import {
+  code,
+  number,
+  country,
+  codes,
+  countries,
+  numbers,
+  data,
+} from "./index.js";
 
-assert(cc.code('EUR').countries.length === 36);
-assert(cc.code('IDR').digits === 2);
-assert(cc.number('967').currency === 'Zambian Kwacha');
-assert(cc.number(967).currency === 'Zambian Kwacha');
-assert(cc.country('Colombia').length === 2);
-assert(cc.country('colombia').length === 2);
-assert(cc.codes().length === 179);
-assert(cc.countries().length === 260);
-assert(cc.numbers().length === 179);
-assert(cc.numbers()[0] === '784');
-assert(cc.data.length == 179);
+assert(code("EUR").countries.length === 36);
+assert(code("IDR").digits === 2);
+assert(number("967").currency === "Zambian Kwacha");
+assert(number(967).currency === "Zambian Kwacha");
+assert(country("Colombia").length === 2);
+assert(country("colombia").length === 2);
+assert(codes().length === 179);
+assert(countries().length === 261);
+assert(numbers().length === 179);
+assert(numbers()[0] === "784");
+assert(data.length == 179);
